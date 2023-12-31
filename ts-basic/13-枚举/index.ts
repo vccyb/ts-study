e26cde81b1232b3762e8a28b8ac6db5672ac24d4
vccyb <13170906656@163.com>
2023-12-31 23:04:03 +0800

    12-31-enum-study

---
+++ ts-basic/13-枚举/index.ts
@@ -0,0 +1,61 @@
+{
+  enum Color {
+    Red,
+    Green,
+    Blue,
+  }
+
+  let c: Color = Color.Green;
+}
+
+const enum Color {
+  Red = 90,
+  Green = 0.5,
+  Blue = 100,
+  x = 1 << 1,
+}
+
+{
+  enum Direction {
+    Up = "UP",
+    Down = "DOWN",
+  }
+}
+
+{
+  const enum MediaTypes {
+    JSON = "application/json",
+    XML = "application/xml",
+  }
+
+  fetch("localhost", {
+    headers: {
+      Accept: MediaTypes.JSON,
+    },
+  }).then((reponse) => {});
+}
+
+{
+  enum MyEnum {
+    A = "a",
+    B = "b",
+  }
+
+  type Foo = keyof typeof MyEnum;
+
+  type Foo2 = { [key in MyEnum]: any };
+}
+
+{
+  enum Weekdays {
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+  }
+
+  console.log(Weekdays[3]); // Wednesday
+}
